f809560bcde486e1b6353ed533cbf04336b70fc9
DinhThienTuTran <kevinthientran2323@gmail.com>
2024-09-11 11:42:17 -0700

    Add multiplecation algorithm, fixes #1

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main(){
   std::cout<< (first+second) << std::endl;
   std::cout<< "Subtraction: "<< first << "-" << second << "=";
   std::cout<< (first-second) << std::endl;
+  std::cout<< "Multiplecation: "<< first << "*" << second << "=";
+  std::cout<< (first*second) << std::endl;
 
   return 0;
 }
