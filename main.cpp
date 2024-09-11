92101b5a8b4c9f7bbf698068ed930aab4147b20b
Nate Smith <natesmitty1738@gmail.com>
2024-09-11 11:30:52 -0700

    add division function

--- main.cpp
+++ main.cpp
@@ -13,5 +13,12 @@ int main(){
   std::cout<< "Subtraction: "<< first << "-" << second << "=";
   std::cout<< (first-second) << std::endl;
 
+  if(second != 0) {
+    std::cout << "Division: " << first << "/" << second << "=";
+    std::cout << (first / second) << std::endl;
+  } else {
+    std::cout << "Division: Cannot divide by zero!" << std::endl;
+  }
+
   return 0;
 }
