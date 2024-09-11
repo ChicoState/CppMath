a3f4e5b48a2dca416c86c7670054e0dc8709c5cf
Feroz Khan <fakhan@csuchico.edu>
2024-09-11 12:02:24 -0700

    Multiplication added

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main(){
   std::cout<< (first+second) << std::endl;
   std::cout<< "Subtraction: "<< first << "-" << second << "=";
   std::cout<< (first-second) << std::endl;
-  // Multiplication
   std::cout<< "Multiplication: "<< first << "*" << second << "=";
   std::cout<< (first*second) << std::endl;
 
