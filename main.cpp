eec5efccbf7a6c831590779e1cff2d8c75a093b4
IvanGav <ivan.gavby@gmail.com>
2024-09-11 11:33:05 -0700

    added binomial coefficient calculation

--- main.cpp
+++ main.cpp
@@ -18,6 +18,28 @@ int main(){
   cout<< (first-second) << endl;
   cout<< "Multiplication: "<< first << "*" << second << "=";
   cout<< (first*second) << endl;
+  cout<< "Binomial coefficient: "<< first << " choose " << second << "=";
+  cout<< (nCr(first,second)) << endl;
 
   return 0;
+}
+
+
+// credit to https://www.geeksforgeeks.org/program-calculate-value-ncr/
+
+// Returns n choose r
+long nCr(int n, int r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+// Returns factorial of n
+long fact(int n)
+{
+      if(n==0)
+      return 1;
+    long res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
 }
