8a138fc971162b77f6b94faa9f67c133b7c063db
AJblu <alexbluebusiness@gmail.com>
2024-09-11 11:34:36 -0700

    Added division section in main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(){
   cout<< (first-second) << endl;
   cout<< "Multiplication: "<< first << "*" << second << "=";
   cout<< (first*second) << endl;
-
+  cout<< "Division: "<< first << "/" << second << "=";
+  cout<< (first/second) << endl;
   return 0;
 }
