da0f2f7f076b4bdee00cd1db0133b444c1a84c3c
Cole <coled.hanna@yahoo.com>
2024-09-12 11:24:44 -0700

    Adding division path in main

--- main.cpp
+++ main.cpp
@@ -14,10 +14,20 @@ int main(){
 
   cout<< "Addition: "<< first << "+" << second << "=";
   cout<< (first+second) << endl;
+  
   cout<< "Subtraction: "<< first << "-" << second << "=";
   cout<< (first-second) << endl;
+  
   cout<< "Multiplication: "<< first << "*" << second << "=";
   cout<< (first*second) << endl;
 
+  if((first != 0) && (second != 0)){
+  cout<< "Division: "<< first << "/" << second << "=";
+  cout<< (first/second) << endl;
+  }
+  else{
+  cout << "Error: Either number can't be Zero, can't do Division." << endl;		  
+  }
+  
   return 0;
 }
